1bd35254257933fa943ae890b3a78c949670c413
AndressaAndrad <andressaandrade.ads@gmail.com>
2022-08-30 12:41:07 -0400

    teste

---
+++ Secao03/desestruturacaoArrays.js
@@ -0,0 +1,11 @@
+const numeros = [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9];
+
+console.log(numeros[2]);
+
+// let a = 'A'; //B
+// let b = 'B'; //C
+// let c = 'C'; //A
+// const numeros = [b,c,a];
+// [a,b,c] = numeros;
+
+// console.log(a,b,c);
