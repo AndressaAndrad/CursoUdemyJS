6a509a9200a925b927378ba449c6a3bd88fe04a6
AndressaAndrad <andressaandrade.ads@gmail.com>
2022-09-27 08:45:12 -0400

    Projeto Agenda final

--- ProjetoAgenda/src/models/ContatoModel.js
+++ ProjetoAgenda/src/models/ContatoModel.js
@@ -66,7 +66,7 @@ Contato.buscaPorId = async function(id) {
 
 Contato.buscaContatos = async function() {
   const contatos = await ContatoModel.find()
-    .sort({ criadoEm: -1 });
+    .sort({ criadoEm: -1 }); // Ordem decrescente
   return contatos;
 };
 
