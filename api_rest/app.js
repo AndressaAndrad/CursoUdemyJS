551120ecf6133b3648fbe42658425900e3c944b2
Andressa Andrade <andressaandrade.ads@gmail.com>
2022-09-29 11:22:00 -0400

    Estruturando projeto

--- api_rest/app.js
+++ api_rest/app.js
@@ -1,3 +1,21 @@
-const teste = 22;
+import express from 'express';
+import homeRoutes from './src/routes/homeRoutes';
 
-console.log(teste);
+class App {
+  constructor() {
+    this.app = express();
+    this.moddlewares();
+    this.routes();
+  }
+
+  moddlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', homeRoutes);
+  }
+}
+
+export default new App().app;
