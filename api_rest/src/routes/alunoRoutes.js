4f874f32c7fccb3722b8dac24770dbdc93377017
Andressa Andrade <andressaandrade.ads@gmail.com>
2022-10-04 09:45:55 -0400

    CRUD alunos

--- api_rest/src/routes/alunoRoutes.js
+++ api_rest/src/routes/alunoRoutes.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import alunoController from '../controllers/AlunoController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
-router.get('/', alunoController.index);
+router.post('/', loginRequired, alunoController.store); // C
+router.get('/', alunoController.index); // R
+router.get('/:id', alunoController.show); // R
+router.put('/:id', loginRequired, alunoController.update); // U
+router.delete('/:id', loginRequired, alunoController.delete); // D
 
 export default router;
